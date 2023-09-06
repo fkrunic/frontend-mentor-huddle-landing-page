07eac8269a1c4b24fb9ae42b95844a08c1dcd763
Filip Krunic <fkrunic9@gmail.com>
2023-09-05 22:17:39 -0700

    desktop page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import Mobile from './components/Mobile.vue'
+import Desktop from './components/Desktop.vue'
 
 onMounted(() => {
-  document.body.className = 'font-poppins bg-violet'
+  document.body.className = 'font-poppins bg-violet overflow-hidden'
 })
 </script>
 
 <template>
   <Mobile class="desktop:hidden"></Mobile>
+  <Desktop class="hidden desktop:flex"></Desktop>
 </template>
---
+++ src/components/Desktop.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-row items-center justify-center gap-12 min-h-screen m-16">
+    <img class="absolute -z-10 -top-1 -right-4 w-[90rem]" src="../assets/bg-desktop.svg">
+    <div class="flex flex-col justify-start gap-20 min-h-screen">
+      <img class="w-[13rem]" src="../assets/logo.svg">
+      <img class="w-[90rem]" src="../assets/illustration-mockups.svg">
+    </div>     
+    <div class="flex flex-col gap-6 pb-32">
+      <p class="text-white font-bold text-[40px] w-[30rem]">
+        Build The Community Your Fans Will Love
+      </p>
+      <p class="text-white text-lg font-open-sans w-[35rem]">
+        Huddle re-imagines the way we build communities. You have a voice, 
+        but so does your audience. Create connections with your users as
+        you engage in genuine discussion.          
+      </p>
+      <div class="
+        flex 
+        flex-row 
+        items-center 
+        justify-center 
+
+        w-[12rem]
+        h-[3.5rem]
+
+        rounded-full
+        shadow-2xl 
+        bg-white
+        
+        cursor-pointer
+        ">
+        <p class="text-lg text-violet">Register</p>
+      </div>
+    </div>
+  </div>
+</template>
